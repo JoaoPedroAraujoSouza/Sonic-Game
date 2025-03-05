ef5060449682cf0db1f0cac7f4023e5d1640ee8d
João Pedro <joao.pedroaraujosouza@ufrpe.br>
2025-03-05 16:32:48 -0300

    feat: add visual da tela jogável a animações

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 document.getElementById("btnStart").addEventListener("click", function() {
-    // Redireciona para a página do jogo
-    window.location.href = "game.html"; 
+    document.querySelector(".tela-inicial").classList.add("fade-out");
+
+    // Redireciona após a animação terminar
+    setTimeout(() => {
+        window.location.href = "game.html";
+    }, 500);
 });
